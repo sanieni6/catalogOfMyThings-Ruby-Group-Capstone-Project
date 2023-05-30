a64cf7060df5ce2bdd9a5f715ea7dff6d7496538
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-05-29 23:23:09 -0400

    Add method to add book in create module

--- modules/create.rb
+++ modules/create.rb
@@ -15,13 +15,25 @@ module Create
     label.add_item(game)
   end
 
-  def add_a_book; end
+  def add_a_book
+    print 'Enter the publisher of the book: '
+    publisher = gets.chomp
+    print 'Enter the cover state of the book: '
+    cover_state = gets.chomp
+    book = Book.new(publisher, cover_state)
+    genre = select_genre
+    genre.add_item(book)
+    author = select_author
+    author.add_item(book)
+    label = select_label
+    label.add_item(book)
+    @books << book
+    puts 'Book created successfully'
+  end
 
   def select_genre; end
 
   def select_author; end
-
-  def select_source; end
 
   def select_label; end
 end
