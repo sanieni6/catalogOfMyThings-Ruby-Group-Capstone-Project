464a6694e14ef853675d641bf1d9e49ba8ebbb85
sanieni6 <luissanchezzamora23@gmail.com>
2023-05-30 11:00:08 -0500

    Add list function for genres

--- modules/list.rb
+++ modules/list.rb
@@ -2,7 +2,7 @@ module List
   def list_all_music_albums
     puts 'No music albums yet, please create an album first' if @music_albums.empty?
     @music_albums.each do |album|
-      puts "Author: #{album.author},  Genre: #{album.genre}"
+      puts "genre: #{album.genre.name},  author: #{album.author.first_name}, Available on spotify:#{album.on_spotify} "
     end
   end
 
@@ -17,6 +17,13 @@ module List
     puts "There's no labels stored yet" if @labels.empty?
     @labels.each do |label|
       puts "Title: #{label.title}, Color: #{label.color}"
+    end
+  end
+
+  def list_all_genres
+    puts "There's no genres stored yet" if @genres.empty?
+    @genres.each do |genre|
+      puts "Genre: #{genre.name}"
     end
   end
 end
