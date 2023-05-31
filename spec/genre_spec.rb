36f19bcce3258ffd688565f8e31a1337510538d5
sanieni6 <luissanchezzamora23@gmail.com>
2023-05-31 10:05:40 -0500

    Fix linters

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,24 +2,23 @@ require_relative '../classes/genre'
 require_relative '../classes/music_album'
 
 describe Genre do
-    genre = Genre.new('Rock')
-    genre.add_id(1)
-    it 'should be an instance of Genre' do
-        expect(genre).to be_instance_of Genre
-    end
+  genre = Genre.new('Rock')
+  genre.add_id(1)
+  it 'should be an instance of Genre' do
+    expect(genre).to be_instance_of Genre
+  end
 
-    it 'should have a name' do
-        expect(genre.name).to eq('Rock')
-    end
+  it 'should have a name' do
+    expect(genre.name).to eq('Rock')
+  end
 
-    it 'should have an id' do
-        expect(genre.id).to eq(1)
-    end
+  it 'should have an id' do
+    expect(genre.id).to eq(1)
+  end
 
-    it 'Should recieve an item' do
-        album = MusicAlbum.new(2019, true)
-        genre.add_item(album)
-        expect(genre.items[0]).to be_instance_of(MusicAlbum)
-    end
+  it 'Should recieve an item' do
+    album = MusicAlbum.new(2019, true)
+    genre.add_item(album)
+    expect(genre.items[0]).to be_instance_of(MusicAlbum)
+  end
 end
-
