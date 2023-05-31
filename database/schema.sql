7d90b4afd948ce76255480b6177ed35124e46216
sanieni6 <luissanchezzamora23@gmail.com>
2023-05-31 11:41:56 -0500

    Fix tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE item (
     id SERIAL PRIMARY KEY,
-    genre VARCHAR(255),
-    author VARCHAR(255),
-    label VARCHAR(255),
+    genre_id INT REFERENCES genre(id),
+    author INT REFERENCES author(id),
+    label INT REFERENCES label(id),
     publish_date DATE,
     archived BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE musicAlbum (
     id SERIAL PRIMARY KEY,
-    genre VARCHAR(255),
-    author VARCHAR(255),
-    label VARCHAR(255),
+    genre_id INT REFERENCES genre(id),
+    author INT REFERENCES author(id),
+    label INT REFERENCES label(id),
     publish_date DATE,
     archived BOOLEAN DEFAULT FALSE,
     on_spotify BOOLEAN DEFAULT FALSE
@@ -20,10 +20,4 @@ CREATE TABLE musicAlbum (
 CREATE TABLE genre (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
-);
-
-CREATE TABLE genre_item (
-    genre_id INT REFERENCES genre(id),
-    item_id INT REFERENCES item(id),
-    PRIMARY KEY (genre_id, item_id)
 );
